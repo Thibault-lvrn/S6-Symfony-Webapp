01fa147c4632cef0bb9197c1e068a9e9695c71bf
thibault livran <thibault.livran@dnd.fr>
2024-06-02 16:12:03 +0200

    generatedPdf: Add base_url parameter to services.yaml

--- src/Controller/GeneratePdfController.php
+++ src/Controller/GeneratePdfController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Doctrine\ORM\EntityManagerInterface;
@@ -15,25 +14,23 @@ use App\Entity\Pdf;
 
 class GeneratePdfController extends AbstractController
 {
-    private $client;
     private $filesystem;
     private $baseUrl;
     private $entityManager;
+    private $microservice_url;
 
-    public function __construct(HttpClientInterface $client, string $baseUrl, EntityManagerInterface $entityManager)
+    public function __construct(string $baseUrl, EntityManagerInterface $entityManager, string $microservice_url)
     {
-        $this->client = $client;
         $this->filesystem = new Filesystem();
         $this->baseUrl = $baseUrl;
         $this->entityManager = $entityManager;
+        $this->microservice_url = $microservice_url;
     }
 
     #[Route('/generate/pdf', name: 'app_generate_pdf')]
     public function form(): Response
     {
         $user = $this->getUser();
-
-        // dd($user);
 
         if (!$this->getUser()) {
             throw new AccessDeniedException('Vous devez être connecté pour générer un PDF.');
@@ -52,7 +49,6 @@ class GeneratePdfController extends AbstractController
         $pdfLimit = $user->getSubscription()->getPdfLimit();
 
         if ($user->getPdfGenerated() >= $pdfLimit) {
-            // throw new AccessDeniedException('Vous avez atteint la limite de PDFs autorisés.');
             return $this->render('error/exeded.html.twig');
         }
 
@@ -66,14 +62,13 @@ class GeneratePdfController extends AbstractController
         $entityManager->persist($user);
         $entityManager->flush();
 
-        // Rediriger vers une page de succès avec le chemin du PDF
         return $this->redirectToRoute('app_success_pdf', ['pdfPath' => $pdfPath]);
     }
 
     private function generatePdf(string $url, string $fileName): string
     {
         $httpClient = HttpClient::create();
-        $response = $httpClient->request('GET', 'http://web/S6-MicroService/public/index.php/generate/pdf', [
+        $response = $httpClient->request('GET', $this->microservice_url, [
             'query' => [
                 'url' => $url,
             ],
@@ -87,7 +82,6 @@ class GeneratePdfController extends AbstractController
         $publicPath = $this->getParameter('kernel.project_dir').'/public/pdf/'.$fileName.'.pdf';
         $pdfUrl = $this->baseUrl.'/public/pdf/'.$fileName.'.pdf';
 
-        // Save the content to a file
         $this->filesystem->dumpFile($publicPath, $content);
 
         $pdf = new Pdf();
@@ -95,7 +89,6 @@ class GeneratePdfController extends AbstractController
         $pdf->setCreatedAt(new \DateTimeImmutable());
         $pdf->setUserId($this->getUser());
 
-        // Persist the Pdf entity
         $this->entityManager->persist($pdf);
         $this->entityManager->flush();
 
